84bc78bab5ac24b837bc8bd8311706b956fb8485
Tanjila <tanjilaakter771746@gmail.com>
2026-07-05 19:55:10 +0600

    remove 1.js file manually

--- app.js
+++ app.js
@@ -73,6 +73,29 @@ function checkAns(idx){
    
 }
 
+function btnPress(){
+    //console.log(this);
+    let btn = this;
+    userFlash(btn);
 
+  let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+
+    console.log(btn.outerHTML);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+} 
+
+function reset(){
+started = false;
+gameSeq = [];
+userSeq = [];
+level = 0;
+}
 
  
